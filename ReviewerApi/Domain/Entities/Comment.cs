b33ffdb5e387d7b5a75abc6f1719b1bd59756d2c
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-17 01:17:42 +0600

    Filling properties for Course and Comment

--- ReviewerApi/Domain/Entities/Comment.cs
+++ ReviewerApi/Domain/Entities/Comment.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Domain.Entities
+namespace Domain.Entities;
+
+public class Comment
 {
-    internal class Comment
-    {
-    }
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public string CourseId { get; set; }
+    public Course Course { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [Range(1, 5)]
+    public short Grade { get; set; }
 }
--- ReviewerApi/Domain/Entities/Course.cs
+++ ReviewerApi/Domain/Entities/Course.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Domain.Entities
+namespace Domain.Entities;
+
+public class Course
 {
-    internal class Course
-    {
-    }
+    public long Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Comment> Comments { get; set; }
 }
