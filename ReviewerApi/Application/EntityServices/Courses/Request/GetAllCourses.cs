26305dfa669b5601d91235581361d8234d75c6f9
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-17 02:06:51 +0600

    Adding Service GetAll for Course

---
+++ ReviewerApi/Application/EntityServices/Courses/Request/GetAllCourses.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.EntityServices.Courses.Request;
+
+public class GetAllCourses
+{
+    public record GetCourseResponse
+    {
+        public long Id { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+    }
+
+    public record GetAllCoursesRequest : IRequest<IEnumerable<GetCourseResponse>> { }
+
+    internal class GetAllBookHandler : IRequestHandler<GetAllCoursesRequest, IEnumerable<GetCourseResponse>>
+    {
+        private readonly ICourseRepo _courseRepo;
+
+        public GetAllBookHandler(ICourseRepo bookRepository)
+        {
+            _courseRepo = bookRepository;
+        }
+
+        public async Task<IEnumerable<GetCourseResponse>> Handle(GetAllCoursesRequest request, CancellationToken cancellationToken)
+        {
+            var Courses = await _courseRepo.GetAllAsync().ConfigureAwait(false);
+            var response = new List<GetCourseResponse>();
+            foreach (var course in Courses)
+            {
+                var result = new GetCourseResponse()
+                {
+                    Id = course.Id,
+                    Name = course.Name,
+                    Description = course.Description,
+                };
+                response.Add(result);
+            }
+            return response;
+        }
+    }
+}
