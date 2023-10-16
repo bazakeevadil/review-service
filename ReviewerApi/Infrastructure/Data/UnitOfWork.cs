e9de61757f2740c21296d42d0b5b7dacfb0f698c
abazakeev <bazakeev04@gmail.com>
2023-10-16 19:19:07 +0600

    Adding UnitOfWork

---
+++ ReviewerApi/Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Application.Shared;
+
+namespace Infrastructure.Data;
+
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task CommitAsync()
+    {
+        return _context.SaveChangesAsync();
+    }
+}
