521448dd77cb675c371c480af3c5bfd20f71bb91
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-18 00:27:18 +0600

    Add Dto for Comment and Course

---
+++ ReviewerApi/Application/Contract/CommentDto.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Contract;
+
+public class CommentDto
+{
+    public long Id { get; set; }
+    public long UserId { get; set; }
+    public User? User { get; set; }
+    public string? CourseId { get; set; }
+    public Course? Course { get; set; }
+    public string? Content { get; set; }
+    public short Grade { get; set; }
+}
---
+++ ReviewerApi/Application/Contract/CourseDto.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Contract;
+
+public class CourseDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public List<Comment> Comments { get; set; } = new();
+}
