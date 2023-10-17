ba5043d4354274b7eca2a2b196c9e0e36d1d19a3
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-18 00:27:57 +0600

    Added Prop for UserDto

--- ReviewerApi/Application/Contract/UserDto.cs
+++ ReviewerApi/Application/Contract/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Enum;
+﻿using Domain.Entities;
+using Domain.Enum;
 
 namespace Application.Contract;
 
@@ -7,4 +8,5 @@ public record UserDto
     public required Guid Id { get; init; }
     public required string Username { get; init; }
     public required Role Role { get; init; }
+    public List<Comment> Comments { get; set; } = new();
 }
