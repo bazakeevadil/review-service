ba42ce592550cd6788e9d4817ffbab6591f438ea
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-18 21:36:35 +0600

    Filled Data For Repos for Comment and Course

---
+++ ReviewerApi/Infrastructure/Repositories/CommentRepo.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class CommentRepo : ICommentRepo
+{
+    private readonly AppDbContext _context;
+
+    public CommentRepo(AppDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public void Add(Comment entity)
+    {
+        _context.Comments.Add(entity);
+    }
+
+    public void DeleteByName(Comment entity)
+    {
+        _context.Comments.Remove(entity);
+    }
+
+    public async Task DeleteCommentByIdAsync(long id)
+    {
+        var entity = await _context.Comments.FindAsync(id);
+
+        if (entity is not null)
+            _context.Comments.Remove(entity);
+    }
+
+    public Task<List<Comment>> GetAllAsync()
+    {
+        return _context.Comments.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Comment?> GetCommentById(long id)
+    {
+        var entity = await _context.Comments.FindAsync(id);
+
+        if (entity != null)
+            _context.Entry(entity).State = EntityState.Detached;
+
+        return entity;
+    }
+
+    public void Update(Comment entity)
+    {
+        _context.Comments.Update(entity);
+    }
+}
