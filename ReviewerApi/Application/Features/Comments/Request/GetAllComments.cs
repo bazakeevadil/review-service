6df73778cbece7f1e6126888aeabdae62d39e96b
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-19 00:21:02 +0600

    Added Services For Comment

--- ReviewerApi/Application/Features/Comments/Request/GetAllComments.cs
+++ ReviewerApi/Application/Features/Comments/Request/GetAllComments.cs
@@ -1,5 +1,29 @@
-﻿namespace Application.Features.Comments.Request;
+﻿using Application.Contract;
+using Domain.Repositories;
+using Mapster;
+using MediatR;
 
-public class GetAllComments
+namespace Application.Features.Comments.Request;
+
+public record GetAllCommentsQuery : IRequest<List<CommentDto>> { }
+
+internal class GetAllCommentQueryHandler
+    : IRequestHandler<GetAllCommentsQuery, List<CommentDto>>
 {
+    private readonly ICommentRepo _courseRepository;
+
+    public GetAllCommentQueryHandler(ICommentRepo commentRepository)
+    {
+        _courseRepository = commentRepository;
+    }
+
+    public async Task<List<CommentDto>> Handle(
+        GetAllCommentsQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _courseRepository.GetAllAsync();
+
+        var response = users.Adapt<List<CommentDto>>();
+
+        return response;
+    }
 }
