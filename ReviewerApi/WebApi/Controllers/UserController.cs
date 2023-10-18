12674e48e953270f3267077f934658fde28c9062
abazakeev <bazakeev04@gmail.com>
2023-10-18 15:35:18 +0600

    Changes

--- ReviewerApi/WebApi/Controllers/UserController.cs
+++ ReviewerApi/WebApi/Controllers/UserController.cs
@@ -1,6 +1,9 @@
-﻿namespace WebApi.Controllers
-{
-    public class UserController
-    {
-    }
-}
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController, Route("api/user")]
+public class UserController : ControllerBase
+{ }
+
+
