762088b4b9ddc452cb2df915f3827d49c386a311
Zhyrgalbek1 <137794850+Zhyrgalbek1@users.noreply.github.com>
2023-10-20 01:57:32 +0600

    Created GlobalUsings

---
+++ ReviewerApi/Application/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Application.Features.Comments.Command;
+global using Application.Contract;
+
+global using Domain.Entities;
+global using Domain.Repositories;
+
+global using MediatR;
+global using Mapster;
